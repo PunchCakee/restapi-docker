6339bc0f7fb2da4182da8fd6dc05d6c927808154
Faisal alghamdi <faisal.alghamdi357@gmail.com>
2022-12-04 22:49:59 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI
+import json
+import constants
+
+app = FastAPI()
+
+
+@app.get("/update/")
+async def root(key: str, container: str):
+    if key == constants.API_KEY:
+        with open("images.json",'r') as file:
+            data = json.load(file)
+            for container_itr in data:
+                print(container_itr)
+                if container_itr['container'] == container:
+                    return {"container": container}
+                else:
+                    return {"ERROR": "CONTAINER NOT FOUND"}
+    else:
+        return {"ERROR":"INVALID KEY"}
+
+@app.get("/")
+async def main():
+    return {"it":"works!"}
+
